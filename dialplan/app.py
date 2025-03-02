3b42605b75baf5533dfd671e35990052f0edc287
Faheem <faheem2084@gmail.com>
2025-03-02 14:20:32 +0400

    freeswitch directory from remote http server
    freeswitch directory from remote http server

--- dialplan/app.py
+++ dialplan/app.py
@@ -3,8 +3,8 @@ from fastapi.responses import Response
 
 app = FastAPI()
 
-@app.get("/")
-@app.post("/")  # Allow both GET and POST
+@app.get("/dialplan")
+@app.post("/dialplan")  # Allow both GET and POST
 def get_dialplan():
     xml_content = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <document type="freeswitch/xml">
@@ -23,3 +23,29 @@ def get_dialplan():
 </document>
 """
     return Response(content=xml_content, media_type="application/xml")
+
+
+@app.get("/directory")
+@app.post("/directory")  # Endpoint for directory
+def get_directory():
+    xml_content = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<document type="freeswitch/xml">
+  <section name="directory" description="Remote Directory">
+    <domain name="$${domain}">
+      <user id="1000">
+        <params>
+          <param name="password" value="1234"/>
+          <param name="vm-password" value="1000"/>
+        </params>
+        <variables>
+          <variable name="user_context" value="default"/>
+          <variable name="effective_caller_id_name" value="Extension 1000"/>
+          <variable name="effective_caller_id_number" value="1000"/>
+        </variables>
+      </user>
+    </domain>
+  </section>
+</document>
+"""
+    return Response(content=xml_content, media_type="application/xml")
+
